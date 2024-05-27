37f15687e7b7bd804f0bf64e51957070238a55c0
Meenal Garg (Credera) <meenal.g@credera.com>
2024-05-27 17:31:40 +0530

    site-selector, lang selector

--- src/sxastarter/src/components/Components Library/SiteSelector.tsx
+++ src/sxastarter/src/components/Components Library/SiteSelector.tsx
@@ -1,4 +1,6 @@
+import { event } from "@sitecore-cloudsdk/events/browser";
 import { ComponentProps } from "lib/component-props";
+import { useState } from "react";
 type SiteSelector = {
     text: string,
     value: string,
@@ -12,12 +14,23 @@ type SiteSelectorProps = ComponentProps & {
     fields: Fields;
 };
 export const Default = (props: SiteSelectorProps): JSX.Element => {
+    const datadropdown = props.fields.items;
+    const [select, setSelect] = useState('1');
+    const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelect(event.target.value);
+        window.location.href = event.target.value;
+      };
     console.log(props.fields)
     return (
-        <div className="">
-            <span>
-                Site Selector
-            </span>
-        </div>
+        <div className="site--selector">
+        <select className="site-selector-select" value = {select} onChange={handleSelect}>
+           
+         {datadropdown.map(option =>(
+             <option  value = {option.value}>{option.text}</option>
+         ))}
+          <option value ="https://www.google.com/">google</option>
+        </select>
+        </div> 
     );
+
 }
