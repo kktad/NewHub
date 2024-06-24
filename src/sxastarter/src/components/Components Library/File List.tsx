64d34c54d184e87fc3fcb3f1104fb9d301f0257d
Meenal Garg (Credera) <meenal.g@credera.com>
2024-06-24 17:42:14 +0530

    file list

--- src/sxastarter/src/components/Components Library/File List.tsx
+++ src/sxastarter/src/components/Components Library/File List.tsx
@@ -1,4 +1,7 @@
+import React from "react";
 import { ComponentProps } from "lib/component-props";
+import ModalImage from "react-modal-image"; // Import react-modal-image package
+import download from "downloadjs"; // Import downloadjs for file downloads
 
 type FileItems = {
     file:{
@@ -34,12 +37,42 @@ type FileListsProps = ComponentProps & {
     fields: Fields;
 };
 
-export const Default = (props: FileListsProps): JSX.Element => {    
+const FileListsComponent: React.FC<FileListsProps> = (props) => {
     console.log(props.fields.data.datasource.FileLists.FileDetails);
-    console.log(props.params.TitleHeadingLevel);
+
+    const handleDownload = (imageUrl: string, fileName: string) => {
+        // Use downloadjs to initiate the download
+        download(imageUrl, fileName);
+    };
+
     return (
         <div className="main">
-            File List Component
-      </div>
+            {/* Render your file list items */}
+            <p className="example-text">Example file list</p>
+            {props.fields.data.datasource.FileLists.FileDetails.map((fileItem, index) => (
+                <div key={index} className="file-item">
+                    {/* Display thumbnail */}
+                    <ModalImage
+                        small="https://aautio.github.io/react-modal-image/example_img_small.jpg" // Thumbnail image
+                        large="https://aautio.github.io/react-modal-image/example_img_small.jpg" // Full-size image URL
+                        alt={fileItem.customImage.field.value.alt} // Alt text for accessibility
+                        hideDownload={true} // Hide default download button from ModalImage
+                        hideZoom={true} // Hide default zoom button from ModalImage
+                        className="thumbnail"
+                    />
+                    {/* Download button */}
+                    <button
+                        onClick={() => handleDownload(fileItem.file.url, fileItem.customImage.field.value.alt)}
+                        className="download-btn"
+                    >
+                        {fileItem.linkText.value}  {/* Display filename or customize as needed */}
+                    </button>
+                    
+                    <p>{fileItem.description.value}</p>
+                </div>
+            ))}
+        </div>
     );
-}
+};
+
+export default FileListsComponent;
