2cd5640a5a91e67f578c88bea6a618b0010f36a0
Meenal Garg (Credera) <meenal.g@credera.com>
2024-06-26 16:05:50 +0530

    accordian

--- src/sxastarter/src/components/Components Library/Accordion.tsx
+++ src/sxastarter/src/components/Components Library/Accordion.tsx
@@ -1,11 +1,13 @@
 import { RichTextField } from "@sitecore-jss/sitecore-jss-nextjs";
 import { ComponentProps } from "lib/component-props";
+import Collapsible from 'react-collapsible';
+import { useState } from 'react';
 
 type AccordionItem = {
     heading: RichTextField,
     content: RichTextField
-
 }
+
 interface Fields {
     settings: {
         expandOnHover: boolean,
@@ -17,15 +19,67 @@ interface Fields {
     },
     items: AccordionItem[]
 }
+
 type AccordionProps = ComponentProps & {
     fields: Fields;
 };
 
-export const Default = (props: AccordionProps): JSX.Element => {
-    console.log(props.fields)
+const Default: React.FC<AccordionProps> = ({ fields, params }) => {
+    console.log(params, 'mee')
+    const [isOpenIndexes, setIsOpenIndexes] = useState<number[]>([]);
+
+    const canOpenMultipleCustom = fields.settings.canOpenMultiple;
+    const canToggleCustom = fields.settings.canToggle;
+    const expandOnHoverCustom = fields.settings.expandOnHover;
+    const expandedByDefaultCustom = fields.settings.expandedByDefault;
+    const speedCustom = fields.settings.speed;
+
+    const settings = {
+        canOpenMultiple: canOpenMultipleCustom,
+        canToggle: canToggleCustom,
+        expandOnHover: expandOnHoverCustom,
+        expandedByDefault: expandedByDefaultCustom,
+        speed: speedCustom,
+    };
+
+    const handleToggle = (index: number) => {
+        setIsOpenIndexes(prevIndexes => {
+            if (prevIndexes.includes(index)) {
+                return prevIndexes.filter(i => i !== index);
+            } else {
+                return [...prevIndexes, index];
+            }
+        });
+    };
+
     return (
         <div>
-            Accordion Component
+            {fields.items.map((item, index) => {
+                const triggerText = item.heading ? item.heading.toString() : '';
+                const displayText = item.content ? item.content.toString() : '';
+
+                const isOpen = isOpenIndexes.includes(index);
+                const accordionClass = params.Styles === 'accordion-horizontal' ? 'horizontal' : '';
+                const customClass = params.Styles === 'accordion-horizontal' ? 'custom-class-for-horizontal' : '';
+
+                return (
+                    <Collapsible  
+                        key={`collapsible-${index}`}
+                        trigger={triggerText}
+                        transitionTime = {speedCustom}
+                        open ={expandedByDefaultCustom}
+                        className={`Collapsible ${accordionClass} ${isOpen ? 'is-open' : ''} ${customClass}`}
+                        {...settings}
+                        openedClassName={`Collapsible ${accordionClass} ${customClass} is-open`} // Specify openedClassName to maintain classes when opened
+                        onOpening={() => handleToggle(index)}
+                        onClosing={() => handleToggle(index)}
+                    >
+                        <div>{displayText}</div>
+                    </Collapsible>
+                );
+            })}
         </div>
     );
-}
+};
+
+export default Default;
