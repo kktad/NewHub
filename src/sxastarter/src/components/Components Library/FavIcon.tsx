cc87f94d8321c1883990543353f112fc05a477bb
Debasis Mishra <debasis.mishra@credera.com>
2024-07-08 18:29:48 +0530

    XCWD-81 - Fav Icon - BE - Bug Fixes

--- src/sxastarter/src/components/Components Library/FavIcon.tsx
+++ src/sxastarter/src/components/Components Library/FavIcon.tsx
@@ -1,4 +1,4 @@
-import { ImageField, TextField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ImageField, TextField, Image as JssImage, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 interface Fields {
@@ -9,6 +9,9 @@ type FavIconProps = ComponentProps & {
   fields: Fields;
 };
 
+const imageLabel = 'Fav Icon Image: ';
+const imageCaptionLabel = 'Fav Icon Image Caption: ';
+
 export const Default = (props: FavIconProps): JSX.Element => {
   console.log('Props: ', props);
 
@@ -17,6 +20,13 @@ export const Default = (props: FavIconProps): JSX.Element => {
       <hr />
       Fav Icon Component
       <br />
+      <br />
+      <div>{imageLabel}</div>
+      <JssImage field={props.fields.Image} />
+      <br />
+      <br />
+      <div>{imageCaptionLabel}</div>
+      <Text field={props.fields.ImageCaption} />
     </div>
   );
 };
